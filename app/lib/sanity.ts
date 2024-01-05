c178bc6b3e9f1b5ecd0a08f8d0cf18d131ff6c88
Etxebe <mikelgartzia21@gmail.com>
2024-01-05 02:01:19 +0100

    [NEW] creado fichero Hero.tsx, problemas al crear schemas en administrador localhost

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "pz9ehg2k",
+  projectId: "u9o3gxov",
   dataset: "production",
   apiVersion: "2022-03-25",
   useCdn: true,
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
+import Hero from "./components/Hero";
+//import Newest from "./components/Newest";
+
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello from the main page</h1>
+    <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      <Hero />
+      
     </div>
-  )
+  );
 }
